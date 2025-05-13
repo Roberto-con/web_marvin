66f3d9d392c1ba83959d095818db11499d5ae78d
Roberto Mario Condori Gutiérrez <roberto@MacBook-Pro-de-Roberto.local>
2025-05-12 22:52:09 -0400

    Feat: Agregado soporte para importar excel12

--- backend/app.py
+++ backend/app.py
@@ -34,7 +34,7 @@ def get_db_connection():
         port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
         buffered=True
     )
 
